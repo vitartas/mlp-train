767185f5abe2074376d992162c97119e06384bdf
Valdas Vitartas <vitartc@gmail.com>
2023-04-21 16:58:27 +0100

    Add ORCA potential

--- mlptrain/potentials/__init__.py
+++ mlptrain/potentials/__init__.py
@@ -1,7 +1,9 @@
 from mlptrain.potentials.gap.gap import GAP
 from mlptrain.potentials.ace.ace import ACE
 from mlptrain.potentials.nequip._nequip import NequIP
+from mlptrain.potentials.orca import OrcaPotential
 
 __all__ = ['GAP',
            'ACE',
-           'NequIP']
+           'NequIP',
+           'OrcaPotential']
---
+++ mlptrain/potentials/orca.py
@@ -0,0 +1,43 @@
+import os
+from mlptrain.potentials._base import MLPotential
+from ase.calculators.orca import ORCA
+
+class OrcaPotential(MLPotential):
+
+    def __init__(self,
+                 name: str,
+                 charge: int,
+                 mult: int,
+                 orcasimpleinput: str,
+                 orcablocks: str,
+                 system=None):
+
+        super().__init__(name=name, system=system)
+
+        self.charge = charge
+        self.mult = mult
+
+        # Values for the calculator
+        self.orcasimpleinput = orcasimpleinput
+        self.orcablocks = orcablocks
+
+        # ORCA v5.0.3
+        self.path = '/usr/local/orca_5_0_3/orca'
+        os.environ['ASE_ORCA_COMMAND'] = f'{self.path} PREFIX.inp > PREFIX.out'
+
+    @property
+    def ase_calculator(self):
+
+        return ORCA(charge=self.charge,
+                    mult=self.mult,
+                    orcasimpleinput=self.orcasimpleinput,
+                    orcablocks=self.orcablocks)
+
+    def _train(self) -> None:
+        """ABC for MLPotential required but unused in OrcaPotential"""
+
+    def requires_atomic_energies(self) -> None:
+        """ABC for MLPotential required but unused in OrcaPotential"""
+
+    def requires_non_zero_box_size(self) -> None:
+        """ABC for MLPotential required but unused in OrcaPotential"""
