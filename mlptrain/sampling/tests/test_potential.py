adf2f4859c89671eb64c709f12a920e3d4f1496c
Valdas Vitartas <vitartc@gmail.com>
2023-05-04 18:41:03 +0100

    Modify test potential

--- mlptrain/sampling/tests/test_potential.py
+++ mlptrain/sampling/tests/test_potential.py
@@ -36,15 +36,24 @@ class DoubleWellPotential(Calculator):
 
     __test__ = False
 
+    def __init__(self, k, a1, a2):
+        Calculator.__init__(self)
+        self.k = k
+        self.a1 = a1
+        self.a2 = a2
+
     def get_potential_energy(self, atoms):
 
         r = atoms.get_distance(0, 1)
 
-        return (r - 1)**2 * (r - 4)**2
+        return self.k * (r - self.a1)**2 * (r - self.a2)**2
 
     def get_forces(self, atoms):
 
         derivative = np.zeros((len(atoms), 3))
+        k = self.k
+        a1 = self.a1
+        a2 = self.a2
 
         r = atoms.get_distance(0, 1)
         if r < 10**(-7):
@@ -58,7 +67,7 @@ class DoubleWellPotential(Calculator):
         derivative[0] = [x_i, y_i, z_i]
         derivative[1] = [-x_i, -y_i, -z_i]
 
-        force = -2 * derivative * (r - 1) * (r - 4) * (2*r - 5)
+        force = -2 * k * derivative * (r - a1) * (r - a2) * (2*r - a1 - a2)
 
         return force
 
@@ -70,10 +79,12 @@ class TestPotential(MLPotential):
     def __init__(self,
                  name: str,
                  calculator='harmonic',
-                 system=None):
+                 system=None,
+                 **kwargs):
 
         super().__init__(name=name, system=system)
         self.calculator = calculator.lower()
+        self.kwargs = kwargs
 
     @property
     def ase_calculator(self):
@@ -82,7 +93,7 @@ class TestPotential(MLPotential):
             return HarmonicPotential()
 
         if self.calculator == 'doublewell':
-            return DoubleWellPotential()
+            return DoubleWellPotential(**self.kwargs)
 
         if self.calculator == 'lj':
             return LennardJones(rc=2.5, r0=3.0)
