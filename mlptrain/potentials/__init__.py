00f845cde3d2b13d5807e4927345804db23aa36a
Valdas Vitartas <vitartc@gmail.com>
2023-06-20 09:39:20 +0100

    Add ORCA potential

--- mlptrain/potentials/__init__.py
+++ mlptrain/potentials/__init__.py
@@ -2,8 +2,10 @@ from mlptrain.potentials.gap.gap import GAP
 from mlptrain.potentials.ace.ace import ACE
 from mlptrain.potentials.nequip._nequip import NequIP
 from mlptrain.potentials.mace.mace import MACE
+from mlptrain.potentials.orca import OrcaPotential
 
 __all__ = ['GAP',
            'ACE',
            'NequIP',
-           'MACE']
+           'MACE',
+           'OrcaPotential']
